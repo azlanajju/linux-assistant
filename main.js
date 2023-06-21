093c7cbe842ae9715b0b6844ea45d43cd2faaf9f
azlanajju <muhammedazlan11@gmail.com>
2023-06-21 22:08:13 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+  const commandSets = document.querySelectorAll(".commandsSet");
+
+searchInput.addEventListener("keyup", function () {
+  const searchTerm = searchInput.value.toLowerCase();
+
+  // Iterate through each command set
+  for (const commandSet of commandSets) {
+    const tags = commandSet.getAttribute("data-tags").toLowerCase();
+    const commandDiv = commandSet;
+
+    // Show or hide the command set based on the search term and tags
+    if (
+      tags.includes(searchTerm) ||
+      commandSet.querySelector("textarea").value.toLowerCase().includes(searchTerm)
+    ) {
+      commandDiv.style.display = "block";
+    } else {
+      commandDiv.style.display = "none";
+    }
+  }
+});
